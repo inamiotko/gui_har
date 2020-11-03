847466b3551439f7a43f62e79eb8d20f0490498f
Iga <56221715+inamiotko@users.noreply.github.com>
2020-11-03 15:14:27 +0100

    Add files via upload
    added fft to data visualization, added legend in few charts and added handling of csv data containing collected steps

--- gui_project.py
+++ gui_project.py
@@ -101,14 +101,17 @@ class MainApplication(tk.Frame):
             for activity, percentage in zip(predictions_label[0::2], predictions_label[1::2]):
                 results = results + activity + " " + percentage + "\n" + " "
             nlines = results.count('\n')
-            if nlines >= 4: label_height = 8
-            else: label_height = 5
+            if nlines >= 4:
+                label_height = 8
+            else:
+                label_height = 5
             self.label_results.config(anchor='nw', justify='left', wraplength='550', height=label_height,
                                       text=f'Recognized Human Activities: \n {results}')
 
     def browse_files_graph(self):
         try:
-            self.filename = filedialog.askopenfilename(filetypes=(('Physical Activity data files', '.json .txt .npy'),))
+            self.filename = filedialog.askopenfilename(
+                filetypes=(('Physical Activity data files', '.json .txt .npy .csv'),))
             self.graphs_chooser['state'] = 'active'
             file = os.path.basename(self.filename)
             self.label_files_img.config(fg='black', text=f'Chosen file: {file}')
@@ -154,11 +157,33 @@ class MainApplication(tk.Frame):
                 df_c = np.load(self.filename)
                 self.graphs_chooser['values'] = graphs
 
+            elif self.filename.endswith('.csv'):
+                graphs = ['amount of steps throughout the time period',
+                          'intensity of movement throughout the days']
+                self.graphs_chooser['values'] = graphs
+                df = pd.read_csv(self.filename, sep=',')
+                df[['date', 'time']] = df["Time"].str.split(" ", 1, expand=True)
+                df['day_number'] = df.groupby('date').ngroup()
+                df[['hours', 'minutes']] = df["time"].str.split(":", 1, expand=True)
+                df['minutes'] = df['minutes'].str.split(':', 1).str[0]
+                df['hours'] = df['hours'].astype(int)
+                df['minutes'] = df['minutes'].astype(int)
+                df['oneday_time_mins'] = df['hours'] * 60 + df['minutes']
+                df['overall_time_mins'] = df['day_number'] * 24 * 60 + df['hours'] * 60 + df['minutes']
+                df.drop(columns=['Time'])
+                data = df[['Steps', 'day_number', 'oneday_time_mins']]
+                data = data.pivot(index='day_number', columns='oneday_time_mins', values='Steps')
+                data[np.isnan(data)] = 0
+                data_st = df.groupby('day_number')['Steps'].sum().reset_index()
+
             else:
                 if 'WISDM' in self.filename:
                     graphs = ['acceleration data combined', 'acceleration data X', 'acceleration data Y',
                               'acceleration data Z',
-                              'number of samples per activity']
+                              'number of samples per activity', 'FFT spectrum for jogging',
+                              'FFT spectrum for walking', 'FFT spectrum for walking upstairs',
+                              'FFT spectrum for walking downstairs', 'FFT spectrum for standing',
+                              'FFT spectrum for sitting']
                     self.graphs_chooser['values'] = graphs
                     columns = ['user-id', 'activity', 'timestamp', 'x-axis', 'y-axis', 'z-axis']
                     numb = 20
@@ -167,6 +192,13 @@ class MainApplication(tk.Frame):
                                      names=columns)
                     df['z-axis'].replace(';', '', regex=True, inplace=True)
                     df['z-axis'] = df['z-axis'].astype('float64')
+                    jog = df[df['activity'] == 'Jogging']
+                    walk = df[df['activity'] == 'Walking']
+                    stand = df[df['activity'] == 'Standing']
+                    sit = df[df['activity'] == 'Sitting']
+                    up = df[df['activity'] == 'Upstairs']
+                    down = df[df['activity'] == 'Downstairs']
+
                 else:
                     graphs = ['acceleration data combined', 'acceleration data X', 'acceleration data Y',
                               'acceleration data Z']
@@ -182,6 +214,7 @@ class MainApplication(tk.Frame):
             self.label_files_img.config(fg='red', text='You need to choose file')
 
         def callback(event):
+
             if self.filename.endswith(
                     '.json') and self.graphs_chooser.get() == 'amount of steps throughout the time period':
                 fig = plt.figure(figsize=(8, 5))
@@ -214,15 +247,74 @@ class MainApplication(tk.Frame):
                 ax.set_xlabel('Days')
                 ax.set_ylabel('Time of day')
 
+            elif self.filename.endswith('.csv') and \
+                    self.graphs_chooser.get() == 'intensity of movement throughout the days':
+                fig, ax = plt.subplots(figsize=(8, 5))
+                ax = sns.heatmap(data, cbar_kws={'label': 'Intensity'}, xticklabels=False)
+                ax.invert_yaxis()
+                ax.set_xlabel('Minutes in one day period')
+                ax.set_ylabel('Number of the day')
+
+            elif self.filename.endswith('.csv') and \
+                    self.graphs_chooser.get() == 'amount of steps throughout the time period':
+                fig = plt.figure(figsize=(8, 5))
+                ax = fig.add_subplot(111)
+                ax.bar(data_st['day_number'], data_st['Steps'])
+                ax.set_ylabel('Total number of steps')
+                ax.set_xlabel('Number of the day')
+
             else:
+                def fft_graph(dataframe):
+                    sampling_freq = 20
+                    graph_list_x = dataframe['x-axis'].tolist()
+                    graph_list_y = dataframe['y-axis'].tolist()
+                    graph_list_z = dataframe['z-axis'].tolist()
+                    fourierTransform_x = np.fft.fft(graph_list_x) / len(graph_list_x)
+                    fourierTransform_x = fourierTransform_x[range(int(len(graph_list_x) / 2))]
+                    fourierTransform_y = np.fft.fft(graph_list_y) / len(graph_list_y)
+                    fourierTransform_y = fourierTransform_y[range(int(len(graph_list_y) / 2))]
+                    fourierTransform_z = np.fft.fft(graph_list_z) / len(graph_list_z)
+                    fourierTransform_z = fourierTransform_z[range(int(len(graph_list_z) / 2))]
+                    tpCount_x = len(graph_list_x)
+                    tpCount_y = len(graph_list_y)
+                    tpCount_z = len(graph_list_z)
+                    values_x = np.arange(int(tpCount_x / 2))
+                    values_y = np.arange(int(tpCount_y / 2))
+                    values_z = np.arange(int(tpCount_z / 2))
+                    timePeriod_x = tpCount_x / sampling_freq
+                    timePeriod_y = tpCount_y / sampling_freq
+                    timePeriod_z = tpCount_z / sampling_freq
+                    frequencies_x = values_x / timePeriod_x
+                    frequencies_y = values_y / timePeriod_y
+                    frequencies_z = values_z / timePeriod_z
+                    ax.plot(frequencies_x, abs(fourierTransform_x), label='x')
+                    ax.plot(frequencies_y, abs(fourierTransform_y), label='y')
+                    ax.plot(frequencies_z, abs(fourierTransform_z), label='z')
+                    ax.legend()
+                    ax.set_xlabel('Frequency')
+                    ax.set_ylabel('Amplitude')
+
                 fig, ax = plt.subplots(figsize=(8, 5))
                 x = df['x-axis'][:numb]
                 y = df['y-axis'][:numb]
                 z = df['z-axis'][:numb]
                 if self.graphs_chooser.get() == 'acceleration data combined':
-                    ax.plot(x)
-                    ax.plot(y)
-                    ax.plot(z)
+                    ax.plot(x, label='x')
+                    ax.plot(y, label='y')
+                    ax.plot(z, label='z')
+                    ax.legend()
+                elif self.graphs_chooser.get() == 'FFT spectrum for jogging':
+                    fft_graph(jog)
+                elif self.graphs_chooser.get() == 'FFT spectrum for walking':
+                    fft_graph(walk)
+                elif self.graphs_chooser.get() == 'FFT spectrum for walking upstairs':
+                    fft_graph(up)
+                elif self.graphs_chooser.get() == 'FFT spectrum for walking downstairs':
+                    fft_graph(down)
+                elif self.graphs_chooser.get() == 'FFT spectrum for standing':
+                    fft_graph(stand)
+                elif self.graphs_chooser.get() == 'FFT spectrum for sitting':
+                    fft_graph(sit)
                 elif self.graphs_chooser.get() == 'acceleration data X':
                     ax.plot(x)
                 elif self.graphs_chooser.get() == 'acceleration data Y':
